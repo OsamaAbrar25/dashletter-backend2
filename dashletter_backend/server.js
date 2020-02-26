337a011a75f85f2235a345b918fe4ed0b3a8dfd4
satyaom <satyaom1998@gmail.com>
2020-02-26 11:48:44 +0530

    create server.js and build for frontend

---
+++ dashletter_backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = 4000
+
+//including client side file
+app.use(express.static(path.join(__dirname, '../dashletter_frontend-master/build')))
+
+//starting fronteOsamaAbrar25/dashletter-backend2.gitnd
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../dashletter_frontend-master/build', 'index.html'))
+})
+
+//hosting at localhost:4000
+app.listen(port, (err)=>{
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(`server started at ${port}`)
+    }
+})
---
+++ dashletter_frontend-master/build/precache-manifest.e21ba64b14888a7936f71ce801eff348.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d8ddd074e0f59958c4a20a98a3965e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ae6395e219f4a687a53",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6953df6676f283e9809b",
+    "url": "/static/css/main.40c5ab71.chunk.css"
+  },
+  {
+    "revision": "1ae6395e219f4a687a53",
+    "url": "/static/js/2.cf3616b3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.cf3616b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6953df6676f283e9809b",
+    "url": "/static/js/main.a8b5f3c2.chunk.js"
+  },
+  {
+    "revision": "8f9567c486e157184f17",
+    "url": "/static/js/runtime-main.accd7f18.js"
+  },
+  {
+    "revision": "a7a61faf2cfdbe584a22547932339b30",
+    "url": "/static/media/1tile.a7a61faf.png"
+  },
+  {
+    "revision": "b6200cb4c052a39606122a8e0687ed6a",
+    "url": "/static/media/2tile.b6200cb4.png"
+  },
+  {
+    "revision": "5cc4359d6d869192fbc3e4bd0947f656",
+    "url": "/static/media/3tile.5cc4359d.png"
+  },
+  {
+    "revision": "ec192e19ac3ea87502c1b7913d9c5818",
+    "url": "/static/media/4tile.ec192e19.png"
+  },
+  {
+    "revision": "c40f2298bf663ff67f5f72f934e33d38",
+    "url": "/static/media/a.c40f2298.jpeg"
+  },
+  {
+    "revision": "306871d3a3a00ecade29526ed29c5581",
+    "url": "/static/media/c.306871d3.jpg"
+  },
+  {
+    "revision": "72587e781c12f2beb8f92c980f2db189",
+    "url": "/static/media/cards (1).72587e78.jpg"
+  },
+  {
+    "revision": "e4728c108a4f04645d41db8dad2eb96a",
+    "url": "/static/media/cards (1).e4728c10.png"
+  },
+  {
+    "revision": "e4cbd2a5eb53c1f256e9534bc0aba73b",
+    "url": "/static/media/cards (10).e4cbd2a5.jpg"
+  },
+  {
+    "revision": "1a33ad3033e25201648dac4088402d36",
+    "url": "/static/media/cards (11).1a33ad30.jpg"
+  },
+  {
+    "revision": "39100cdea9fb6921abf3e79e44cad050",
+    "url": "/static/media/cards (12).39100cde.jpg"
+  },
+  {
+    "revision": "104893acf8f3a8b047121d29dac2b1a1",
+    "url": "/static/media/cards (2).104893ac.png"
+  },
+  {
+    "revision": "f5241a392377e1f3c68bfe9428940045",
+    "url": "/static/media/cards (2).f5241a39.jpg"
+  },
+  {
+    "revision": "b0c52e331ab573d850b3e9970a9c4d75",
+    "url": "/static/media/cards (3).b0c52e33.jpg"
+  },
+  {
+    "revision": "1ffbd86e509827208101b743269c3830",
+    "url": "/static/media/cards (4).1ffbd86e.jpg"
+  },
+  {
+    "revision": "a4330c3e1e4ef1a6a2a1c10b34ae022b",
+    "url": "/static/media/cards (5).a4330c3e.jpg"
+  },
+  {
+    "revision": "187e7998b638a4532259d9fe3a513497",
+    "url": "/static/media/cards (6).187e7998.jpg"
+  },
+  {
+    "revision": "be2139ca8381823dd5ed4e2a4beb3ff8",
+    "url": "/static/media/cards (7).be2139ca.jpg"
+  },
+  {
+    "revision": "1c16b56e4b97ef33b865b1ffdd51c0ec",
+    "url": "/static/media/cards (8).1c16b56e.jpg"
+  },
+  {
+    "revision": "e533053a1901d261910e5a5e5ae74e10",
+    "url": "/static/media/cards (9).e533053a.jpg"
+  },
+  {
+    "revision": "deb95b218eef9108e31c4cbc07303884",
+    "url": "/static/media/e.deb95b21.jpg"
+  }
+]);
