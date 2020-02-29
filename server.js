4fc6393fa4e7bc4ce7eeb790b24b63e1a69b3f37
satyaom <satyaom1998@gmail.com>
2020-02-29 19:30:58 +0530

    removing /users

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-app.get('/', (req, res)=>{
-    res.send('hello bhai')
-})
 
 var Users = require('./routes/user')
-app.use('/users', Users)
+app.use(Users)
 
 //hosting at localhost:4000
 app.listen(port, (err)=>{
