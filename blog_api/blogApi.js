281bbc832fc2a52873d01a2b057a01934228232e
satyaom <satyaom1998@gmail.com>
2020-04-03 17:50:00 +0530

    update: splited json file to 18 category blog

--- blog_api/blogApi.js
+++ blog_api/blogApi.js
@@ -1,23 +1,27 @@
 var axios = require('axios');
 var fs = require('fs')
 
+ //list of files_blog
+ files_blog = ['../asset/category/fashion.json', '../asset/category/food.json', '../asset/category/travel.json', '../asset/category/music.json', '../asset/category/lifestyle.json', '../asset/category/fitness.json', '../asset/category/DIY.json', '../asset/category/sports.json', '../asset/category/finance.json', '../asset/category/political.json', '../asset/category/parenting.json', '../asset/category/business.json', '../asset/category/personal.json', '../asset/category/movie.json', '../asset/category/car.json', '../asset/category/news.json', 
+ '../asset/category/pet.json', '../asset/category/gaming.json']
+
+var j = 0
 
 //list of queries
-query = ['fashion blog', 'food blog', 'travel blog', 'music blog', 'lifestyle blog', 'fitness blog', 'DIY blog', 'sports blog', 'finance blog',
- 'political blog', 'parenting blog', 'business blog', 'personal blog', 'movie blog', 'car blog', 'news blog', 'pet blog', 'gaming blog']
+query = ['fashion clothing blog', 'food blog', 'travel blog', 'music blog', 'lifestyle blog', 'fitness blog', 'DIY blog', 'sports blog', 'finance blog',
+ 'political blog', 'parenting blog', 'business blog', 'personal blog', 'movie blog', 'car blog', 'news blog', 'pet dog cat blog', 'gaming blog']
 
 async function processGet() {
-    for (const element of query) {
-        //removing blog from query
-        search = element.replace(' blog', '');
+    for (i in query) {
+        
         var url = 'http://newsapi.org/v2/everything?'+
-            `q=${element}&` +
-            'sortBy=relevance&'+
+            `q=${query[i]}&` +
+            'sortBy=popularity&'+
             'language=en&'+
             'totalResults=100&'+
             'pageSize=100&'+
             'apiKey=450cb76834d643e2ba8896df71043fb6';
-
+        
         //get request newsapi
         await axios.get(url)
         .then(function (response) {
@@ -26,32 +30,38 @@ async function processGet() {
             //arranging data according domain name
             contents = categoryOfData(contents);
             //reading processed.json
-            data = fs.readFileSync('./asset/blog.json');
-            // parsing data from string to json
-            data = JSON.parse(data);
-            //selecting query(like fashion)
-            category = data[search];
-            //adding blog to particular category
-            for (var key in contents) {
-                if (category.hasOwnProperty(key)) {
-                    for (var keyGet in category) {
-                            if (key == keyGet) {
-                                category[key].concat(contents[key]);
-                            }    
-                    }  
-                } else {
-                    category[key] = contents[key];
-                }     
+            data = fs.readFileSync(files_blog[j]);
+            
+            if (data.length != 0) {
+                // parsing data from string to json
+                category = JSON.parse(data);
+                //adding blog to particular category
+                for (var key in contents) {
+                    if (category.hasOwnProperty(key)) {
+                        for (var keyGet in category) {
+                                if (key == keyGet) {
+                                    category[key].concat(contents[key]);
+                                }    
+                        }  
+                    } else {
+                        category[key] = contents[key];
+                    }     
+                }
+                //writing to file
+                fs.writeFileSync(files_blog[j], JSON.stringify(category));  
+            } else {
+                fs.writeFileSync(files_blog[j], JSON.stringify(contents));
             }
-            //writing to file
-            fs.writeFileSync('./asset/blog.json', JSON.stringify(data));  
+            
         })
         .catch(function (error) {
         // handle error
             console.log(error);
-        });       
+        });   
+        j++;    
     }
     console.log('done');
+    
 }
 
 
@@ -76,3 +86,8 @@ function categoryOfData(data) {
 }
 
 setInterval(processGet, 86400000);
+
+
+
+
+
