4c95592871c5fd077fb0ce7074c3de03b569b27c
satyaom <satyaom1998@gmail.com>
2020-04-08 23:52:30 +0530

    test

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const randomstring = require('randomstring');
 
 con = require('../database/connection');
 
-users.use(cors({allowedHeaders:['set-cookie'], exposedHeaders:['set-cookie']}));
+users.use(cors({allowedHeaders:['Content-Type','set-cookie'], exposedHeaders:['set-cookie']}));
 
 //starting frontend
 users.get('/', (req, res)=>{
