c05db82f8db4eb2df8521fb577ba9206895479c7
satyaom <satyaom1998@gmail.com>
2020-04-09 21:11:43 +0530

    test

--- routes/user.js
+++ routes/user.js
@@ -64,14 +64,14 @@ users.post('/signup', (req, res)=>{
         var sql = `select * from crendential where email like "${user_email}"`;
         con.query(sql, (err, result)=>{
             if (err) {
-                console.log(err.message);
+                console.log(err);
             } else if(result.length == 0) {
                 var sql = `insert into crendential values("${token}", "${user_email}", "${user_password}")`;
                 var sql_user = `insert into user_detail values("${token}","${user_name}","${user_gender}","${user_dob}", "${user_country}")`;
                 async function userInsert(){
                     await con.query(sql_user, (err)=>{
                         if(err) {
-                            console.log(err.message);
+                            console.log(err);
                         } else {
                             res.json({message:'user details inserted'});
                         }
@@ -81,7 +81,7 @@ users.post('/signup', (req, res)=>{
                 
                 con.query(sql, (err)=>{
                     if (err) {
-                        console.log(err.message)
+                        console.log(err)
                     } else {
                         res.json({message:'credentials inserted'});
                     }
