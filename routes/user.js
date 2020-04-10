1badda1af064bf323a00f5829261846ec098486e
satyaom <satyaom1998@gmail.com>
2020-04-11 03:36:22 +0530

    test

--- routes/user.js
+++ routes/user.js
@@ -127,7 +127,7 @@ users.get('/blog', (req, res)=>{
 users.get('/confirmation/:token', async (req, res) => {
     try {
       var payload = jwt.verify(req.params.token, 'asdf1093KMnzxcvnkljvasdu09123nlasdasdf');
-      sql = `UPDATE crendential SET confirmed =  WHERE email = "${payload.id}"`;
+      sql = `UPDATE crendential SET confirmed = true WHERE email = "${payload.id}"`;
       await con.query(sql);
       res.send(payload.id);
 
